c2d824680017c754b2ac3257f2e501e774c035a0
rudransh <rudranshsharma123@gmail.com>
2022-03-20 18:09:55 +0530

    Update navbar.js

--- frontend_DeSo/src/views/navbar/navbar.js
+++ frontend_DeSo/src/views/navbar/navbar.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
+import Deso from "deso-protocol";
+
 function Navbar() {
+	const deso = new Deso();
+	const [publicKey, setPublicKey] = useState("");
+	const [notfis, setNotifs] = useState("");
+	const fectNotifs = async () => {
+		const request = {
+			PublicKeyBase58Check: publicKey,
+		};
+		const response = await deso.notification.getUnreadNotificationsCount(
+			request,
+		);
+		console.log(response.data.NotificationsCount);
+		setNotifs(response.data.NotificationsCount);
+	};
+	useEffect(() => {
+		try {
+			// toastySuccess("checking to see if you have already logged in");
+			const key = deso.identity.getUserKey();
+			if (key !== "" && key !== null) {
+				setPublicKey(key);
+				// toastySuccess("you have already logged in, check footer for details");\
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (publicKey) {
+			fectNotifs();
+		}
+	}, [publicKey]);
+
 	return (
 		<nav>
 			<div className="nav-links">
@@ -11,6 +45,7 @@ function Navbar() {
 				<a href="/">
 					<button className="cta-button mint-button"> Home</button>
 				</a>
+
 				<a href="/ens">
 					<button className="cta-button mint-button"> ENS</button>
 				</a>
@@ -23,6 +58,10 @@ function Navbar() {
 				<a href="/board">
 					<button className="cta-button mint-button"> Board</button>
 				</a>
+				<button className="cta-button mint-button">
+					{" "}
+					Number of Notifs {notfis}
+				</button>
 			</div>
 		</nav>
 	);
